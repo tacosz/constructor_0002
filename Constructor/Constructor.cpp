e1e5f1cc7c83c147eb6177e2f7d36a849dec01b9
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-14 18:47:22 +0700

    construktor mahasiswa inim dan inama dalam akses modifier public

--- Constructor/Constructor.cpp
+++ Constructor/Constructor.cpp
@@ -13,11 +13,20 @@ public:
     {
         nim = 0;
         nama = "";
-    }
+    };
     Mahasiswa(int iNim)
     {
         nim = iNim;
-    }
+    };
+    Mahasiswa(string iNama)
+    {
+        nama = iNama;
+    };
+    Mahasiswa(int iNim, string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    };
 };
 
 int main()
