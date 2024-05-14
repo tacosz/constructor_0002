a4a6cd806607327080214a9f7d9831eaba30ddb1
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-14 17:27:30 +0700

    akses modifier public dalam class mahasiswa

--- MemberStatik/MemberStatik.cpp
+++ MemberStatik/MemberStatik.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+using namespace std;
+
+class mahasiswa
+{
+public:
+    static int nim;
+    int id;
+    string nama;
+
+    void setID();
+
+    
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
