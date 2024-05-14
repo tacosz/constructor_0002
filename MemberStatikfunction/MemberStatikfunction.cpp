9434e14baa8e98230d3a6f041fd05aff412cc76c
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-14 18:22:13 +0700

    parameter mahasiswa

--- MemberStatikfunction/MemberStatikfunction.cpp
+++ MemberStatikfunction/MemberStatikfunction.cpp
@@ -16,6 +16,12 @@ public:
 
     static void setNim(int pNim) { nim = pNim; }
     static int getNim() { return nim; }
+
+    mahasiswa(string pnama)
+    {
+        nama = pnama;
+        setID();
+    }
 };
 
 int main()
